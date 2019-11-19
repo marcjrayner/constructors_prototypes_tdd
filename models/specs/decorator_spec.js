0d33a654dea6fea3cc9d7a98f4e881fa8a1340c6
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-19 20:46:09 +0000

    adds decorator, starts with empty paintStock array

--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,2 +1,14 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js')
+
+describe ('Decorator', function () {
+  let decorator
+  beforeEach(function() {
+    decorator = new Decorator()
+  });
+
+  it('should start with empty paint stock', function () {
+    const actual = decorator.paintStock;
+    assert.deepStrictEqual(actual, [])
+  })
+})
