d707c4f75b999f07d991883fc079dfe09c802203
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-19 18:32:45 +0000

    room painted status starts false - pass

--- models/specs/room_spec.js
+++ models/specs/room_spec.js
@@ -5,9 +5,15 @@ describe('Room', function () {
   let room
   beforeEach(function () {
     room = new Room(10);
-  })
+  });
+
   it('should have an area', function () {
     const actual = room.area;
     assert.strictEqual(actual, 10)
   });
+
+  it('should start unpainted', function () {
+    const actual = room.painted;
+    assert.strictEqual(actual, false)
+  })
 });
