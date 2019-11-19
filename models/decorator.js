8b8fbf5b87e1935f6109b085687e58d5496f4637
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-19 20:58:50 +0000

    decorator can count total paint volume

--- models/decorator.js
+++ models/decorator.js
@@ -1,5 +1,17 @@
 const Decorator = function () {
   this.paintStock = []
-}
+};
+
+Decorator.prototype.addPaint = function (paint) {
+  this.paintStock.push(paint);
+};
+
+Decorator.prototype.totalPaintVolume = function () {
+  let total = 0
+  for (var paint of this.paintStock) {
+    total += paint.volume
+  }
+  return total
+};
 
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,14 +1,33 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js')
+const Paint = require('../paint.js')
 
 describe ('Decorator', function () {
   let decorator
+  let paint1
+  let paint2
   beforeEach(function() {
-    decorator = new Decorator()
+    decorator = new Decorator();
+    paint1 = new Paint(5);
+    paint2 = new Paint(10)
   });
 
   it('should start with empty paint stock', function () {
     const actual = decorator.paintStock;
     assert.deepStrictEqual(actual, [])
-  })
-})
+  });
+
+  it('should be able to add a paint to paintStock', function() {
+    decorator.addPaint(paint1)
+    const actual = decorator.paintStock;
+    assert.deepStrictEqual(actual, [paint1])
+  });
+
+  it('should be able to count total volume of paint in stock', function() {
+    decorator.addPaint(paint1)
+    decorator.addPaint(paint2)
+    const actual = decorator.totalPaintVolume();
+    assert.deepStrictEqual(actual, 15)
+  });
+
+});
