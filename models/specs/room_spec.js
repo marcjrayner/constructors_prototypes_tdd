47c2e063959328227be811e0cc52ed5736d5570f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-19 18:30:42 +0000

    room has area - pass

--- models/specs/room_spec.js
+++ models/specs/room_spec.js
@@ -1,2 +1,13 @@
 const assert = require('assert');
-const Room = require('../room.js')
+const Room = require('../room.js');
+
+describe('Room', function () {
+  let room
+  beforeEach(function () {
+    room = new Room(10);
+  })
+  it('should have an area', function () {
+    const actual = room.area;
+    assert.strictEqual(actual, 10)
+  });
+});
